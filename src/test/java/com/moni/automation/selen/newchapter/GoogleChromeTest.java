10a5da484aed82582ca03675de10b2f7b752055f
MohanrajRamalingam <mohanrajram@yahoo.co.in>
2018-01-23 23:04:37 -0600

    Minor update - 1

--- src/test/java/com/moni/automation/selen/newchapter/GoogleChromeTest.java
+++ src/test/java/com/moni/automation/selen/newchapter/GoogleChromeTest.java
@@ -16,29 +16,33 @@ public class GoogleChromeTest {
 	private SeleniumHq seleniumHq;
 	private Documentation documentationPage;
 
-	@Before
-	public void setUp() throws Exception {
-		System.setProperty("webdriver.gecko.driver", "./geckodriver.exe");
-		webDriver = new FirefoxDriver();
-		webDriver.manage().window().maximize();
-		webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	}
+//	@Before
+//	public void setUp() throws Exception {
+//		System.setProperty("webdriver.gecko.driver", "./geckodriver.exe");
+//		webDriver = new FirefoxDriver();
+//		webDriver.manage().window().maximize();
+//		webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+//	}
 
 //	@After
 //	public void tearDown() throws Exception {
 //		webDriver.quit();
 //	}
 
+//	@Test
+//	public void testOne() throws Exception {
+//		SeleniumHq seleniumHq = new SeleniumHq(webDriver);
+//		Documentation documentPage = seleniumHq.clickDocumentation();
+//		WebDriverDocumentation wdDocumentation = documentPage.navigateToWebDriverDocumentation();
+//
+//		String actualTitle = webDriver.getTitle();
+//		String expectedTitle = "Selenium WebDriver — Selenium Documentation";
+//		assertEquals(expectedTitle, actualTitle);
+//
+//	}
+
 	@Test
 	public void testOne() throws Exception {
-		SeleniumHq seleniumHq = new SeleniumHq(webDriver);
-		Documentation documentPage = seleniumHq.clickDocumentation();
-		WebDriverDocumentation wdDocumentation = documentPage.navigateToWebDriverDocumentation();
-
-		String actualTitle = webDriver.getTitle();
-		String expectedTitle = "Selenium WebDriver — Selenium Documentation";
-		assertEquals(expectedTitle, actualTitle);
-
+		System.out.println("Test");
 	}
-
 }
